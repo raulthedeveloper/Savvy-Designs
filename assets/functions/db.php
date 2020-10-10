cb84786ab4bedc0f1bdc4fa55aeeffa42c56c3ef
SIabo13 <raul0882@gmail.com>
2020-10-10 19:41:20 -0400

    new commit

--- assets/functions/db.php
+++ assets/functions/db.php
@@ -4,17 +4,8 @@
 $servername = "localhost";
 $username = "raultper_siabo";
 $password = "P9!%gvrDTG%n";
-<<<<<<< HEAD
-$database = "raultper_savy_design";
-
-// $servername = "localhost";
-// $username = "admin";
-// $password = "";
-// $database = "savvy_design";
-=======
 $database = "raultper_portfolio";
 
->>>>>>> 18a9ee5f5190a32426286162c33bb92ce6040536
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -25,10 +16,6 @@ if (!$conn) {
 }
 
 
-<<<<<<< HEAD
-$sql = "SELECT * FROM team";
-=======
 $sql = "SELECT * FROM work_items";
->>>>>>> 18a9ee5f5190a32426286162c33bb92ce6040536
 $result = mysqli_query($conn, $sql);
 ?>
